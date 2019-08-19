ebe2f81602111add92bd9b5a9544d9947a7c0ddc
emilio barrera <barrera_emilio@hotmail.com>
2019-08-19 19:23:50 +0100

    Export file to CSV

---
+++ BlazorCRUD/Client/Helpers/IJSExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Client.Helpers
+{
+    public static class IJSExtension
+    {
+        public static Task GuardarComo(this IJSRuntime js , string nombreArchivo, byte[] archivo)
+        {
+            return js.InvokeAsync<object>("saveAsFile", nombreArchivo, Convert.ToBase64String(archivo));
+        }
+    }
+}
---
+++ BlazorCRUD/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,10 @@
+﻿function saveAsFile(fileName,bytesBase64) {
+
+    var link = document.createElement('a');
+    link.download = fileName;
+    link.href = "data:application/actect-stream;base64," + bytesBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+}
